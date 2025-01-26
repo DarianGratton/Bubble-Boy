e890ec6fc9ecf1361f42365100fe01a592d76b9e
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2025-01-26 14:17:21 -0800

    bubble lifespan added

--- Assets/BubblePU.cs
+++ Assets/BubblePU.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 //collision handle
 public class BubblePU : MonoBehaviour
 {
+    void Start(){
+        StartCoroutine(lifetime());
+    }
+
+    IEnumerator lifetime(){
+        yield return new WaitForSeconds(60f);
+        Destroy(gameObject);
+    }
     public void AddSpeed(float newY){
         float speedMagnitude = 0.65f;
         gameObject.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, newY * speedMagnitude,0);
