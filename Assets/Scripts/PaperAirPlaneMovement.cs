6ab3c8f511491484c0ac5c265b5f16d2af014bdb
Darian Gratton <25462660+DarianGratton@users.noreply.github.com>
2025-01-26 00:08:24 -0800

    Added jank airplane movement logic

--- Assets/Scripts/PaperAirPlaneMovement.cs
+++ Assets/Scripts/PaperAirPlaneMovement.cs
@@ -4,18 +4,23 @@ public class PaperAirPlaneMovement : MonoBehaviour
 {
     [Header("Configuration")]
     public float timeGliding = 2.0f;
-    public float timeDescending = 5.0f;
-    private float descendingAcceleration = 5.0f;
+    public float timeDescending = 2.0f;
+    private float descendingAcceleration = 1.0f;
+    private float ascendingAcceleration = 0.2f;
 
     private Rigidbody rb;
+    private Rigidbody cameraRb;
     private float timeElapsed = 0.0f;
     private bool isGliding = true;
-    private float acceleration = 0.0f;   
+    private float acceleration = 0.0f;
+    private Vector3 startingPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        cameraRb = Camera.main.GetComponent<Rigidbody>();
+        startingPos = gameObject.transform.position;
     }
 
     // Update is called once per frame
@@ -35,10 +40,11 @@ public class PaperAirPlaneMovement : MonoBehaviour
         }
         else if (!isGliding && timeElapsed > timeDescending)
         {
-            acceleration = 0.0f;
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0);
+            acceleration = ascendingAcceleration;
             isGliding = true;
             timeElapsed = 0.0f;
         }
-        rb.AddRelativeForce(Vector3.up * acceleration);
+        rb.AddForce(Vector3.up * acceleration);
     }
 }
